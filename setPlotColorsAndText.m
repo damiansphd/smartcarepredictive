5e52a2bc8f3d9e3bebce72526e1c598f1b6f4a8f
damiansphd <37808691+damiansphd@users.noreply.github.com>
2019-01-28 14:42:25 +0000

    Fix to plots for new features

--- setPlotColorsAndText.m
+++ setPlotColorsAndText.m
@@ -3,35 +3,46 @@ function [combinedmask, plottext, left_color, lint_color, right_color, rint_colo
 % setPlotColorsAndText - sets the plot title text and colors based on
 % measures feature mask settings
 
-combinedmask = measurerow.RawMeas|| measurerow.BucketMeas || measurerow.Range || measurerow.Volatility;
+combinedmask = measurerow.RawMeas|| measurerow.BucketMeas || measurerow.Range ...
+                || measurerow.Volatility || measurerow.AvgSeg || measurerow.VolSeg || measurerow.CChange;
 
-if (measurerow.RawMeas|| measurerow.BucketMeas || measurerow.Range)
-        left_color = [0, 0.65, 1];
-        lint_color = 'red';
-    else
-        left_color = [0.83, 0.83, 0.83];
-        lint_color = [0.83, 0.83, 0.83];
-    end
-    if measurerow.Volatility
-        right_color = [0.13, 0.55, 0.13];
-        rint_color = 'red';
-    else
-        right_color = [0.83, 0.83, 0.83];
-        rint_color = [0.83, 0.83, 0.83];
-    end
+if (measurerow.RawMeas|| measurerow.BucketMeas || measurerow.Range || measurerow.AvgSeg || measurerow.CChange)
+    left_color = [0, 0.65, 1];
+    lint_color = 'red';
+else
+    left_color = [0.83, 0.83, 0.83];
+    lint_color = [0.83, 0.83, 0.83];
+end
+if (measurerow.Volatility || measurerow.VolSeg)
+    right_color = [0.13, 0.55, 0.13];
+    rint_color = 'red';
+else
+    right_color = [0.83, 0.83, 0.83];
+    rint_color = [0.83, 0.83, 0.83];
+end
     
-    plottext = measurerow.DisplayName{1};
-    if measurerow.RawMeas
-        plottext = sprintf('%s-M', plottext);
-    end
-    if measurerow.BucketMeas
-        plottext = sprintf('%s-BM', plottext);
-    end
-    if measurerow.Range
-        plottext = sprintf('%s-R', plottext);
-    end
-    if measurerow.Volatility
-        plottext = sprintf('%s-V', plottext);
-    end
+plottext = measurerow.DisplayName{1};
+if measurerow.RawMeas
+    plottext = sprintf('%s-M', plottext);
+end
+if measurerow.BucketMeas
+    plottext = sprintf('%s-BM', plottext);
+end
+if measurerow.Range
+    plottext = sprintf('%s-R', plottext);
+end
+if measurerow.Volatility
+    plottext = sprintf('%s-V', plottext);
+end
+if measurerow.AvgSeg
+    plottext = sprintf('%s-AS', plottext);
+end
+if measurerow.VolSeg
+    plottext = sprintf('%s-VS', plottext);
+end
+if measurerow.CChange
+    plottext = sprintf('%s-CC', plottext);
+end
+
 end
 
